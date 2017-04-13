37bcfca5078dc8ce4192d74a57b07b24931bad4c
Zack Chandler <zackchandler@gmail.com>
2017-04-13 09:36:29 -0700

    Working with Typescript integrated with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
 
 module.exports = {
-    entry: './js/app.js',
+    entry: './src/app.tsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-    }
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx']
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+        ]
+    },
+    externals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM'
+    },
+    devtool: 'source-map'
 }
