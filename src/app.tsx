d857f79ff2907eca5df354f993aa3d35cd8376b2
Zack Chandler <zackchandler@gmail.com>
2017-04-13 09:08:52 -0700

    Build works with tsc and webpack as separate commands

---
+++ src/app.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Layout} from './layout';
+
+ReactDOM.render(<Layout />, document.getElementById('app'));
---
+++ src/layout.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export class Layout extends React.Component<never, never> {
+    render() {
+        return <div>Layout</div>;
+    }
+}
